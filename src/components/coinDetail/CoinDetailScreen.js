8d4d67105d3473d966ff5c6cae57cb77d324a8fa
geovanni <gramirez@ideacodex.co>
2021-06-30 09:13:16 -0600

    Sesion 13- Creando una FlatList horizontal

--- src/components/coinDetail/CoinDetailScreen.js
+++ src/components/coinDetail/CoinDetailScreen.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text, Image, StyleSheet, SectionList } from 'react-native';
+import { View, Text, Image, StyleSheet, FlatList, SectionList } from 'react-native';
 import colors from '../../res/colors';
+import Http from '../../libs/http';
+import CoinMarketItem from './CoinMarketItem';
 
 class CoinDetailScreen extends Component {
     state = {
-        coin: {}
+        coin: {},
+        markets: []
     }
 
     getSymbolIcon = (name) => {
@@ -34,15 +37,27 @@ class CoinDetailScreen extends Component {
         return section;
     }
 
+    getMarkets = async (coinId) => {
+        const url = `https://api.coinlore.net/api/coin/markets/?id=${coinId}`
+
+        const markets = await Http.instance.get(url);
+
+        this.setState({ markets });
+    }
+
     componentDidMount() {
         const { coin } = this.props.route.params;
+
         this.props.navigation.setOptions({ title: coin.symbol });
+
+        this.getMarkets(coin.id);
+
         this.setState({ coin });
     }
 
     render() {
 
-        const { coin } = this.state;
+        const { coin, markets } = this.state;
 
         return (
             <View style={style.container}>
@@ -51,28 +66,37 @@ class CoinDetailScreen extends Component {
                     <Text style={style.titleText}>
                         {coin.name}
                     </Text>
-
-                    <SectionList
-                        sections={this.getSections(coin)}
-                        keyExtractor={(item) => item}
-                        renderItem={({ item }) =>
-                            <View style={style.sectionItem}>
-                                <Text style={style.sectionText}>{item}</Text>
-                            </View>
-                        }
-                        renderSectionHeader={({ section }) =>
-                            <View style={style.sectionHeader}>
-
-                                <Text style={style.sectionText}>{section.title}</Text>
-                            </View>
-                        }
-                    />
                 </View>
+                <SectionList
+                    style={style.section}
+                    sections={this.getSections(coin)}
+                    keyExtractor={(item) => item}
+                    renderItem={({ item }) =>
+                        <View style={style.sectionItem}>
+                            <Text style={style.sectionText}>{item}</Text>
+                        </View>
+                    }
+                    renderSectionHeader={({ section }) =>
+                        <View style={style.sectionHeader}>
+                            <Text style={style.sectionText}>{section.title}</Text>
+                        </View>
+                    }
+                />
+                <Text style={style.marketsTile}>
+                    Markets
+                </Text>
+                <FlatList
+                    style={style.list}
+                    horizontal={true}
+                    data={markets}
+                    renderItem={({ item }) => <CoinMarketItem item={item} />}
+
+                />
             </View>
         );
     }
 }
-
+//estilos
 const style = StyleSheet.create({
     container: {
         flex: 1,
@@ -93,6 +117,13 @@ const style = StyleSheet.create({
         width: 30,
         height: 30
     },
+    section: {
+        maxHeight: 220
+    },
+    list: {
+        maxHeight: 100,
+        paddingLeft: 16
+    },
     sectionHeader: {
         backgroundColor: "rgba(0,0,0,0.2)",
         padding: 8
@@ -105,5 +136,14 @@ const style = StyleSheet.create({
         fontSize: 14,
         fontWeight: "bold"
     },
+    marketsTile: {
+        color: "white",
+        fontSize: 16,
+        fontWeight: "bold",
+        marginBottom: 16,
+        textAlign: "center"
+    }
 });
+
+
 export default CoinDetailScreen;
